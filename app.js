7d1d34f5c3ed5317836f65eb6e660d03d922d330
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2021-09-26 22:23:17 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+[
+  {
+    name: "screwdriver",
+    strength: "10%",
+    img: "url",
+    citron: true, ice: true, shaker: true,
+    approach: [
+        "hej step 1", "hej step 2"
+    ],
+    favorit: false,
+    ingrediense: [
+      {
+        name: "vodka", amount: 5, category: "alcohol"
+      },
+      {
+        name: "orangejuice", amount: 10, color: "#FFB23E", category: "juice"
+      }
+    ]
+    difficulity: 1
+  }
+]
